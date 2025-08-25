f3a9ba37e4abb5f27f45bf81577dbe5fcdd1deee
Satyendra Shukla <114739647+SatsNik@users.noreply.github.com>
2025-08-26 00:59:33 +0530

    Update wsgi.py

--- aggregator/wsgi.py
+++ aggregator/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+try:
+    from django.core.management import call_command
+    call_command("migrate", interactive=False)
+except Exception as e:
+    print("Migration error:", e)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aggregator.settings')
 
 application = get_wsgi_application()
